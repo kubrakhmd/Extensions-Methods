0f302d829a2d8840f35514089ec380559b06dd8c
kubrakhmd <kubraxe-bp217@code.edu.az>
2024-10-22 23:50:32 +0400

    task added

---
+++ Extensions methods/Extensions methods/Helper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Extensions_methods
+{
+    
+
+
+public static class Helper
+    {
+       
+        public static bool IsOdd(this int number)
+        {
+            return number % 2 != 0;
+        }
+
+     
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+
+       
+        public static bool HasDigit(this string str)
+        {
+            return Regex.IsMatch(str, @"\d");
+        }
+        public static bool CheckPassword(this string password)
+        {
+            if (string.IsNullOrEmpty(password) || password.Length < 8)
+                return false;
+
+            bool hasUpperCase = password.Any(char.IsUpper);
+            bool hasLowerCase = password.Any(char.IsLower);
+            bool hasDigit = password.Any(char.IsDigit);
+
+            return hasUpperCase && hasLowerCase && hasDigit;
+        }
+
+
+
+
+
+        public static string Capitalize(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return str;
+
+            return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+        }
+    }
+
+}
---
+++ Extensions methods/Extensions methods/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Extensions_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+       
+           
+            
+                int number = 5;
+                Console.WriteLine(number.IsOdd());  // true
+                Console.WriteLine(number.IsEven()); // false
+
+                string str = "Hello123";
+                Console.WriteLine(str.HasDigit()); // true
+
+                string password = "Password1";
+                Console.WriteLine(password.CheckPassword()); // true
+
+                string text = "hello world";
+                Console.WriteLine(text.Capitalize()); // "Hello world"
+            
+        }
+
+    }
+}
+
